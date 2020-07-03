cb6c2febf3f2594a8b8a41018d9b185a32cfd827
Moonki Choi <52124204+iknoom@users.noreply.github.com>
2020-07-04 00:51:25 +0900

    Update pattern_generater.py

--- pattern_generater.py
+++ pattern_generater.py
@@ -3,13 +3,13 @@ import numpy as np
 import pickle
 
 class Pattern_generater():
-    def __init__(self, A_pattern, B_pattern, C_pattern): # 가능하면 최대 패턴 입력 갯수 수정하기
+    def __init__(self, A_pattern, B_pattern, C_pattern):
         self.__A_pattern = A_pattern
         self.__B_pattern = B_pattern
         self.__C_pattern = C_pattern
 
     def generate_bfs(self, init_pattern):
-        pattern_database = dict()     # str state : int count_moves / visited 배열이면서 database
+        pattern_database = dict()
         dxs = [1, 0, -1, 0]
         dys = [0, 1, 0, -1]
         pattern_numbers = [n for n in init_pattern if n != 0]
@@ -22,7 +22,7 @@ class Pattern_generater():
 
             for pattern_number in pattern_numbers:
                 move_tile = cur_state.index(pattern_number)
-                i, j = move_tile // 4, move_tile % 4 # 크기 4를 객체에서 받도록 수정하기
+                i, j = move_tile // 4, move_tile % 4
                 for dx, dy in zip(dxs, dys):
                     x, y = i + dx, j + dy
                     new_state = np.array(np.array(cur_state).reshape(4, 4))
@@ -37,7 +37,7 @@ class Pattern_generater():
     def generate_pattern(self):
         print("start generating ...")
 
-        A_pattern_database = self.generate_bfs(self.__A_pattern) # 성공 실패여부 출력하기
+        A_pattern_database = self.generate_bfs(self.__A_pattern)
         database = open("A_database", "wb")
         pickle.dump(A_pattern_database, database)
         database.close()
@@ -59,6 +59,6 @@ class Pattern_generater():
 if __name__ == "__main__":
     A = list(map(int, "1 2 3 0 5 6 0 0 0 0 0 0 0 0 0 0".split()))
     B = list(map(int, "0 0 0 4 0 0 7 8 0 0 11 12 0 0 0 0".split()))
-    C = list(map(int, "0 0 0 0 0 0 0 0 9 10 0 0 13 14 15 0".split())) # 입력 받도록 수정
+    C = list(map(int, "0 0 0 0 0 0 0 0 9 10 0 0 13 14 15 0".split()))
     test_generater = Pattern_generater(A, B, C)
     test_generater.generate_pattern()
